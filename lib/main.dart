c18b5916d672ffdce26c5ed1a2a03125202c7bd8
Vaishnavi Mittal <vishu2001mittal@gmail.com>
2021-09-20 17:15:20 +0530

    feat:Assignment#2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,61 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBallPage(),
       ),
     );
+
+class MagicBallPage extends StatelessWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.blue.shade900,
+        title: Center(child: Text('Ask Me Anything')),
+      ),
+      body: MagicBall(),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+  void randomNum() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: () {
+              print('MagicBall Pressed');
+              setState(() {
+                randomNum();
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
